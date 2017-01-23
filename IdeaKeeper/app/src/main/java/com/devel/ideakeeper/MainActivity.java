8eda7193e9414e9af60138c53cb2255cbe8f47b0
Qu1cK3r <chmolekula@gmail.com>
2017-01-23 17:19:42 +0700

    Added toolbar & search button.

--- IdeaKeeper/app/src/main/java/com/devel/ideakeeper/MainActivity.java
+++ IdeaKeeper/app/src/main/java/com/devel/ideakeeper/MainActivity.java
@@ -2,12 +2,31 @@ package com.devel.ideakeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.app_name);
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        mToolbar.inflateMenu(R.menu.menu);
     }
 }
