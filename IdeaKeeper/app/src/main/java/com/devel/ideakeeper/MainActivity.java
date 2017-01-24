edc4ceefa1ed01e1f17a1e9f4429114d91dc45c6
Qu1cK3r <chmolekula@gmail.com>
2017-01-24 16:16:31 +0700

    NavigationView & tab navigation with test fragments have been implemented.

--- IdeaKeeper/app/src/main/java/com/devel/ideakeeper/MainActivity.java
+++ IdeaKeeper/app/src/main/java/com/devel/ideakeeper/MainActivity.java
@@ -1,21 +1,35 @@
 package com.devel.ideakeeper;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+import com.devel.ideakeeper.adapter.TabsPagerFragmentAdapter;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LAYOUT = R.layout.activity_main;
+
     private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         initToolbar();
+        initNavigationView();
+        initTabs();
     }
 
     private void initToolbar() {
@@ -29,5 +43,42 @@ public class MainActivity extends AppCompatActivity {
         });
 
         mToolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void initTabs() {
+        mViewPager = (ViewPager) findViewById(R.id.viewPager);
+        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(adapter);
+
+        TabLayout mTabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        mTabLayout.setupWithViewPager(mViewPager);
+
+    }
+
+    private void initNavigationView() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                mDrawerLayout, mToolbar, R.string.view_navigation_open, R.string.view_navigation_close);
+        mDrawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                mDrawerLayout.closeDrawers();
+                switch (menuItem.getItemId()) {
+                    case R.id.actionNotificationItem :
+                        showNotificationTab();
+                        break;  //? wtf
+                }
+                return true;
+            }
+        });
+    }
+
+    private void showNotificationTab() {
+        mViewPager.setCurrentItem(Constants.TAB_TWO);
     }
 }
