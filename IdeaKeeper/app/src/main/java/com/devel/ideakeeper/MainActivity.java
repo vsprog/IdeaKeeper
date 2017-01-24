3dfc656f16892376ae233e60df156e1c2e3ea797
Qu1cK3r <chmolekula@gmail.com>
2017-01-24 12:42:27 +0700

    NavigationView implementation.Beginning

--- IdeaKeeper/app/src/main/java/com/devel/ideakeeper/MainActivity.java
+++ IdeaKeeper/app/src/main/java/com/devel/ideakeeper/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
