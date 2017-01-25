ed31118bffb0b2a5bf86dcdd9070d44e67e50b9c
Qu1cK3r <chmolekula@gmail.com>
2017-01-25 15:01:32 +0700

    Configure base project, create simple controller.

---
+++ Idea_Keeper_server/src/main/java/com/devel/ideakeeper/server/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.devel.ideakeeper.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.devel.ideakeeper.server")
+public class WebConfig extends WebMvcConfigurerAdapter {
+}
