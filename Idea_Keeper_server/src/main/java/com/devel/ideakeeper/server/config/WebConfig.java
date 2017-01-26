b31649292493c1f73c61286db4bf4f33fa5b9ccc
Qu1cK3r <chmolekula@gmail.com>
2017-01-26 15:12:02 +0700

    WebConfig edited

--- Idea_Keeper_server/src/main/java/com/devel/ideakeeper/server/config/WebConfig.java
+++ Idea_Keeper_server/src/main/java/com/devel/ideakeeper/server/config/WebConfig.java
@@ -1,13 +1,27 @@
 package com.devel.ideakeeper.server.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.devel.ideakeeper.server")
 public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(new ObjectMapper());
+        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_JSON));
 
+        converters.add(converter);
+    }
 }
