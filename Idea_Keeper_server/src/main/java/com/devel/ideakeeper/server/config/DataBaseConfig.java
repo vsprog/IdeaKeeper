5c11339d708a2bbe86a2ebc6706766dc7deefae1
Qu1cK3r <chmolekula@gmail.com>
2017-01-26 12:52:20 +0700

    Datasource configured.

--- Idea_Keeper_server/src/main/java/com/devel/ideakeeper/server/config/DataBaseConfig.java
+++ Idea_Keeper_server/src/main/java/com/devel/ideakeeper/server/config/DataBaseConfig.java
@@ -1,14 +1,18 @@
 package com.devel.ideakeeper.server.config;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @Configuration
 @EnableJpaRepositories("com.devel.ideakeeper.server.repository")
@@ -20,4 +24,37 @@ public class DataBaseConfig {
     @Resource
     private Environment env;
 
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
+        entityManager.setDataSource(dataSource());
+        entityManager.setPackagesToScan(env.getRequiredProperty("db.entity.package"));
+        entityManager.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManager.setJpaProperties(getHibernateProperties());
+
+        return entityManager;
+    }
+    
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(env.getRequiredProperty("db.url"));
+        dataSource.setDriverClassName(env.getRequiredProperty("db.driver"));
+        dataSource.setUsername(env.getRequiredProperty("db.username"));
+        dataSource.setPassword(env.getRequiredProperty("db.password"));
+        
+        return dataSource;
+    }
+
+    public Properties getHibernateProperties() {
+        try {
+            Properties properties = new Properties();
+            InputStream is = getClass().getClassLoader().getResourceAsStream("hibernate.properties");
+            properties.load(is);
+
+            return properties;
+        }catch (IOException e) {
+            throw new IllegalArgumentException("can not find 'hibernate.roperties' in classpath", e);
+        }
+    }
 }
